4917c201b9b0d64646bbea551ab38cb963334d13
Pedro Sousa <pedro.cruz.sousa@gmail.com>
2017-02-15 14:57:04 +0000

    Use new methods to retrieve data from sqlite

--- controllers/product_by_id.go
+++ controllers/product_by_id.go
@@ -2,11 +2,19 @@ package controllers
 
 import (
 	"fmt"
+	databases "github.com/crazyfacka/pinky-02/databases"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func ProductById(w http.ResponseWriter, request *http.Request, ps httprouter.Params) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", ps.ByName("pid"))
+	sqlite := databases.NewProductsDB("./prod.db")
+	data, err := sqlite.GetProductById(ps.ByName("pid"))
+	if err != nil {
+		w.WriteHeader(err.GetError())
+		fmt.Fprintf(w, "%s", err.ToJSON())
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", data.ToJSON())
+	}
 }
